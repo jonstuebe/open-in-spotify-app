dccf183324cd4feed7f5b019ca4ae9af76a2fa72
Jon Stuebe <jstuebe@gmail.com>
2016-03-23 15:25:47 -0700

    fixing bug

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 'use strict';
 
-$('form').on('submit', function (e) {
+$('form').on('submit', function(e){
 
-	var url = 'https://embed.spotify.com/openspotify/?spuri=' + $('input[type="text]').val() + '&closedelay=5000';
-	$('iframe').attr('src', url);
-
+	var url = 'https://embed.spotify.com/openspotify/?spuri=' + $('#val').val() + '&closedelay=5000';
+	$('iframe').attr('src',url);
+	
 	e.preventDefault();
+	
 });
