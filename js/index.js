fc95a202afe45b05209024a53c08ab9c0a7d6e1c
Jon Stuebe <jsutebe@gmail.com>
2016-03-23 18:43:57 -0700

    updates to support additional format

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
-'use strict';
-
 $('form').on('submit', function(e){
 
-	var url = 'https://embed.spotify.com/openspotify/?spuri=' + $('#val').val() + '&closedelay=5000';
-	$('iframe').attr('src',url);
-	
+	var $input = $('#val'),
+		val = $input.val();
+	var _url = val;
+
+	if(val.indexOf('https://open.spotify.com') == 0)
+	{
+		var regexTrack = /https:\/\/open.spotify.com\/track\/(.*)/;
+		var matches = val.match(regexTrack);
+
+		_url = 'spotify:track:' + matches[1];
+	}
+
+	var embed_url = 'https://embed.spotify.com/openspotify/?spuri=' + _url + '&closedelay=5000';
+	$('iframe').attr('src',embed_url);
+
 	e.preventDefault();
-	
+
 });
