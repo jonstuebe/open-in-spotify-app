8dc0fb6f2f546ccbc11ea8dd779b21fa6d5b2784
Jon Stuebe <jsutebe@gmail.com>
2016-03-25 09:58:13 -0700

    adding url support

--- js/index.js
+++ js/index.js
@@ -1,20 +1,50 @@
-$('form').on('submit', function(e){
+$(document).ready(function() {
 
-	var $input = $('#val'),
-		val = $input.val();
-	var _url = val;
-
-	if(val.indexOf('https://open.spotify.com') == 0)
+	var getQueryVariable = function(variable)
 	{
-		var regexTrack = /https:\/\/open.spotify.com\/track\/(.*)/;
-		var matches = val.match(regexTrack);
-
-		_url = 'spotify:track:' + matches[1];
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+           var pair = vars[i].split("=");
+           if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
 	}
 
-	var embed_url = 'https://embed.spotify.com/openspotify/?spuri=' + _url + '&closedelay=5000';
-	$('iframe').attr('src',embed_url);
+	var convertToEmbed = function(val){
 
-	e.preventDefault();
+		var _url = val;
+		if(val.indexOf('https://open.spotify.com') == 0)
+		{
+			var regexTrack = /https:\/\/open.spotify.com\/track\/(.*)/;
+			var matches = val.match(regexTrack);
+
+			_url = 'spotify:track:' + matches[1];
+		}
+		var embed_url = 'https://embed.spotify.com/openspotify/?spuri=' + _url + '&closedelay=5000';
+		return embed_url;
+	}
+
+	$(window).on('load', function(){
+
+		if(getQueryVariable('url') != "")
+		{
+			$('iframe').attr('src', convertToEmbed(getQueryVariable('url')));
+			window.location.search = '';
+		}
+
+	});
+
+	$('form').on('submit', function(e){
+
+		var $input = $('#val'),
+			val = $input.val();
+
+		$('iframe').attr('src',convertToEmbed(val));
+		$input.val('');
+		e.preventDefault();
+
+	});
+
 
 });
